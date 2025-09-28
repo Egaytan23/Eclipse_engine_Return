81dab6443c41a5b9cdb6e8ab7a6b95b94a015877
Egaytan23 <elvisgaitan23@gmail.com>
2025-09-27 21:11:23 -0400

    movement
    code for player movement complete

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
+
+
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal;
+        Vector3 newVelocity = movement * moveSpeed;
+        newVelocity.y = rb.velocity.y;
+        rb.velocity = newVelocity;
+
+        //Jump Input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
