b28f8b388e644a37361bc47d8a926e30cdc4b07f
Egaytan23 <elvisgaitan23@gmail.com>
2025-11-16 20:48:49 -0500

    unity required

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -43,9 +43,7 @@ public class PlayerMovement : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
-
-    
-
+        
     // Update is called once per frame
     void Update()
     {
